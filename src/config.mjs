c6deff15347d3773dc708ccb647b57cbb619c556
Ivan Ochoa <carlos.ivan10043@gmail.com>
2024-07-07 18:07:51 -0700

    chore: Actualizar referencias a imágenes y nombres de autor

--- src/config.mjs
+++ src/config.mjs
@@ -5,7 +5,7 @@ export default {
     defaultOgImage: "./defaultog.jpg",
     latestPosts: 3,
     postsPerPage: 6,
-    url: "https://pacamara-astro-6y7xr.kinsta.page",
+    url: "https://ciog-blog.vercel.app/",
     social: {
         'dribbble': false,
         'facebook': false,
