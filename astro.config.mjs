ddaff15a8f5fe692b91143da6b705c686600afff
Ivan Ochoa <ivan.ochoa@pricetravel.com>
2024-07-12 20:09:36 -0700

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,7 @@ import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
-
-const baseUrl = process.env.NODE_ENV === 'production' ? process.env.BASE_URL : '/';
-
 export default defineConfig({
   integrations: [tailwind(), mdx()],
   site: "https://ciog.dev/",
-  base: baseUrl,
 });
