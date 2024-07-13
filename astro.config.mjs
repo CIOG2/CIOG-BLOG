9cf122bbf596bf3b5b0ad94cc5ec603151684517
Ivan Ochoa <ivan.ochoa@pricetravel.com>
2024-07-12 19:54:44 -0700

    feat: Actualizar URL del sitio en la configuración de Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,11 @@ import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
+
+const baseUrl = process.env.NODE_ENV === 'production' ? process.env.BASE_URL : '/';
+
 export default defineConfig({
   integrations: [tailwind(), mdx()],
-  site: "https://ciog.dev/"
+  site: "https://ciog.dev/",
+  base: baseUrl,
 });
