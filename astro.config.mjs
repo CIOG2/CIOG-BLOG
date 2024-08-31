3155c16e4ebe625b7de9275f37c6681a635b6cd9
Ivan Ochoa <ivan.ochoa@pricetravel.com>
2024-08-31 14:16:27 -0700

    Se agrego la dependencia de Sitemaps para que se haga automaticamente en lugar de manualmente

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,34 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+
+const site = "https://ciog.dev";
+
+const pagesPriority = [
+  { url: '/', priority: 1 },
+  { url: '/sobre-mi/', priority: 0.9 },
+  { url: '/blog/', priority: 0.8 },
+];
+
+const getPagePriority = (url) => {
+  const page = pagesPriority.find((page) => `${site}${page.url}` === url);
+  return page ? page.priority : 0.7;
+};
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx()],
-  site: "https://ciog.dev/",
+  integrations: [
+    tailwind(),
+    mdx(),
+    sitemap({
+      serialize(item) {
+        item.priority = getPagePriority(item.url);
+        item.lastmod = new Date();
+        item.changefreq = "weekly"; // Default changefreq
+        return item;
+      },
+    }),
+  ],
+  site: site,
 });
